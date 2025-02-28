715d2678f7bbcc64ce1ffaf285cff98bc4ca11d7
YonghaoWang <yonghao-20@mails.tsinghua.edu.cn>
2025-03-01 00:28:30 +0800

    Add distributed train code

--- PretrainScript/guided_diffusion/dist_util.py
+++ PretrainScript/guided_diffusion/dist_util.py
@@ -28,6 +28,7 @@ def setup_dist():
 
     comm = MPI.COMM_WORLD
     backend = "gloo" if not th.cuda.is_available() else "nccl"
+    print(f'use backend {backend}')
 
     if backend == "gloo":
         hostname = "localhost"
