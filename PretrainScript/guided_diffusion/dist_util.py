fd2cbbf0ef40c173ee35b339c5b145cea77a6918
Bilybill <yonghao-16@mails.tsinghua.edu.cn>
2025-03-01 14:36:47 +0800

    Add torch run script

--- PretrainScript/guided_diffusion/dist_util.py
+++ PretrainScript/guided_diffusion/dist_util.py
@@ -11,38 +11,6 @@ from mpi4py import MPI
 import torch as th
 import torch.distributed as dist
 
-# Change this to reflect your cluster layout.
-# The GPU for a given rank is (rank % GPUS_PER_NODE).
-# GPUS_PER_NODE = 8
-
-# SETUP_RETRY_COUNT = 3
-
-
-# def setup_dist():
-#     """
-#     Setup a distributed process group.
-#     """
-#     if dist.is_initialized():
-#         return
-#     os.environ["CUDA_VISIBLE_DEVICES"] = f"{MPI.COMM_WORLD.Get_rank() % GPUS_PER_NODE}"
-#     print(f'CUDA_VISIBLE_DEVICES: {os.environ["CUDA_VISIBLE_DEVICES"]}')
-
-#     comm = MPI.COMM_WORLD
-#     backend = "gloo" if not th.cuda.is_available() else "nccl"
-#     print(f'use backend {backend}')
-
-#     if backend == "gloo":
-#         hostname = "localhost"
-#     else:
-#         hostname = socket.gethostbyname(socket.getfqdn())
-#     os.environ["MASTER_ADDR"] = comm.bcast(hostname, root=0)
-#     os.environ["RANK"] = str(comm.rank)
-#     os.environ["WORLD_SIZE"] = str(comm.size)
-
-#     port = comm.bcast(_find_free_port(), root=0)
-#     os.environ["MASTER_PORT"] = str(port)
-#     dist.init_process_group(backend=backend, init_method="env://")
-
 def setup_dist():
     """
     Setup a distributed process group for use with torchrun.
@@ -67,15 +35,6 @@ def setup_dist():
     # 初始化进程组
     dist.init_process_group(backend=backend)
 
-
-# def dev():
-#     """
-#     Get the device to use for torch.distributed.
-#     """
-#     if th.cuda.is_available():
-#         return th.device(f"cuda")
-#     return th.device("cpu")
-
 def dev():
     """
     Get the device to use for torch.distributed.
@@ -84,53 +43,6 @@ def dev():
         local_rank = int(os.environ.get("LOCAL_RANK", "0"))
         return th.device(f"cuda:{local_rank}")
     return th.device("cpu")
-
-
-# def load_state_dict(path, **kwargs):
-#     """
-#     Load a PyTorch file without redundant fetches across MPI ranks.
-#     """
-#     chunk_size = 2 ** 30  # MPI has a relatively small size limit
-#     if MPI.COMM_WORLD.Get_rank() == 0:
-#         with bf.BlobFile(path, "rb") as f:
-#             data = f.read()
-#         num_chunks = len(data) // chunk_size
-#         if len(data) % chunk_size:
-#             num_chunks += 1
-#         MPI.COMM_WORLD.bcast(num_chunks)
-#         for i in range(0, len(data), chunk_size):
-#             MPI.COMM_WORLD.bcast(data[i : i + chunk_size])
-#     else:
-#         num_chunks = MPI.COMM_WORLD.bcast(None)
-#         data = bytes()
-#         for _ in range(num_chunks):
-#             data += MPI.COMM_WORLD.bcast(None)
-
-#     return th.load(io.BytesIO(data), **kwargs)
-
-# def load_state_dict(path, **kwargs):
-#     """
-#     Load a PyTorch file without redundant fetches across ranks.
-#     """
-#     try:
-#         print(f'RANK {dist.get_rank()} loading state dict from {path}')
-#         if dist.get_rank() == 0:
-#             state_dict = th.load(path, **kwargs)
-#         else:
-#             state_dict = None
-#         print(f'Loaded state dict from {path}')
-        
-#         # 广播主节点加载的状态字典到所有其他节点
-#         if dist.get_world_size() > 1:
-#             state_dict_list = [state_dict] if dist.get_rank() == 0 else [None]
-#             dist.broadcast_object_list(state_dict_list, src=0)
-#             state_dict = state_dict_list[0]
-#         print(f'Broadcasted state dict to all nodes')
-        
-#         return state_dict
-#     except Exception as e:
-#         print(f'Error loading state dict: {e}')
-#         raise e
 
 def load_state_dict(path, **kwargs):
     # 设定设备位置
